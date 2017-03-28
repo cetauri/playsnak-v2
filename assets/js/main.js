0dbf3667f5941ebe81bf8fa2626e78aa7e3e497e
Kwon, oh-sang <cetauri@gmail.com>
2017-03-28 19:32:29 +0200

    resizing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,69 @@
+var form = $( "#myform" );
+
+
+$('.nav li').click(function() {
+	$(this).siblings('li').removeClass('active');
+	// $(this).siblings('li').removeClass('active');
+    $(this).addClass('active');
+});
+
+
+
+$( document ).ready(function() {
+	form.validate();
+    console.log( "document loaded" );
+   $(".segment-select").Segment();
+});
+
+$( "#btnSubscribe" ).click(function() {
+	$( "#btnSubscribe" ).blur();
+
+	// https://jqueryvalidation.org/
+	if (!form.valid()){
+		return;
+	}
+	
+	var email = $("#subscribeInputEmail");
+	if(email.val() == ""){
+		email.focus();
+		alert("Please enter a valid email address.");
+		return;
+	}
+
+	alert(":) valid email : " + email.val());
+});
+
+function myMap() {
+
+  var playsnak_positon = {lat: 52.512930, lng: 13.316290};
+  var map = new google.maps.Map(document.getElementById('google-map'), {
+    zoom: 15,
+    center: playsnak_positon,
+    scrollwheel: false,
+    draggable: false
+  });
+
+  var contentString = '<strong>Playsnak GmbH</strong><br/>Bismarckstr. 10, 10625, Germany';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+    position: playsnak_positon,
+    map: map,
+    icon : 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+    title: 'Playsnak GmbH'
+  });
+
+  marker.addListener('click', function() {
+	// infowindow.open(map, marker);
+	map.setCenter(marker.getPosition());
+  });
+
+  infowindow.open(map,marker);
+}
+
 $(function() {
 	
 	// Cache the Window object
@@ -17,7 +83,8 @@ $(function() {
 			
 			// Put together our final background position
 			var coords = '50% '+ yPos + 'px';
-			
+			console.log(coords);
+			console.log($bgobj);
 			// Move the background
 			$bgobj.css({ backgroundPosition: coords });
 			
@@ -25,3 +92,4 @@ $(function() {
 	});
 	
 });
+
